ee01369209c3b24667045a413b34beb9da8e1fea
jossaiCSU <ossai_justin@students.columbusstate.edu>
2023-10-18 02:50:48 -0500

    edited run.py
    edited run.py file (had to main calls by accident)

--- run.py
+++ run.py
@@ -41,35 +41,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-import os
-from functions.schedule_web_scraper import ScheduleWebScraper
-
-def main():
-    print("Hello, and welcome to the class scheduler. Please select an option from the list below.")
-    print()
-    print("a) Parse (Static) Prerequisite file")
-    print("b) Parse DegreeWorksPDF")
-    print("c) Webscrape & parse Class Schedule")
-    
-    choice = input("Enter your choice: ").strip().lower()
-
-    if choice == 'a':
-        #username = input("Press enter to run parser for prereq. courses : ").strip()
-        os.system(f"python3 functions/prereq-parser.py")
-    elif choice == 'b':
-        print("b selected")
-    elif choice == 'c':
-        web_scraper = ScheduleWebScraper()
-        print(web_scraper.formatted_schedule)
-        # os.system(f"python3 functions/schedule_web_scraper.py")       
-    elif choice == 'd':
-        os.system(f"python3 test_all.py")         
-         
-    else:
-        print("Invalid choice. Exiting...")
-
-if __name__ == "__main__":
-    main()
