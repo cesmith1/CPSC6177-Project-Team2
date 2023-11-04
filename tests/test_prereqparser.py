36c631ea4f5cd494249ced3663cf98b9723677f3
Evan Smith <zaratouchiha@gmail.com>
2023-11-04 02:10:30 -0500

    add prereqparser and degreeworksparser test coverage

--- tests/test_prereqparser.py
+++ tests/test_prereqparser.py
@@ -3,6 +3,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 from PyPDF4 import PdfFileReader
+from functions.prereqparser import parsePrereqs
 import json
 #import prereqparser as pp  # assuming your parser code is saved in prereqparser.py
 
@@ -10,8 +11,22 @@ class TestPrereqParser(unittest.TestCase):
 
     def setUp(self):
         # Setup any pre-conditions before running tests, e.g., paths or sample data
-        self.pdf_path = './PrerequisiteGraph-Software_Systems2019-2020.pdf'
-        self.json_output_path = './data/prereqs.json'
+        self.pdf_path = './data/PrerequisiteGraph-Software_Systems2019-2020.pdf'
+        self.json_output_path = './tests/prereqs_temp.json'
+
+    def tearDown(self):
+                # Check if the tests passed
+        if hasattr(self, '_outcome'):  # Python 3.4+
+            result = self._outcome.result
+            if result.wasSuccessful():
+                print(f"{self._testMethodName} passed successfully!")
+        else:  # Python 3.3 and below
+            if not self._resultForDoCleanups.failures and not self._resultForDoCleanups.errors:
+                print(f"{self._testMethodName} passed successfully!")
+        try:
+            os.remove(self.json_output_path)
+        except OSError:
+            pass
 
     def test_pdf_read(self):
         """Test if the PDF file is successfully read."""
@@ -22,21 +37,10 @@ class TestPrereqParser(unittest.TestCase):
                 self.assertGreater(reader.numPages, 0, "No pages detected in the PDF file.")
             print("test_pdf_read passed successfully!")
 
-    def test_course_data_extraction(self):
-        """Test if the course data is successfully extracted."""
-        # assuming you have a function `extract_courses_from_pdf` that does the extraction in prereqparser.py
-        # courses = pp.extract_courses_from_pdf(self.pdf_path)
-        
-        # For this example, I am reading from the JSON output file
-        with open(self.json_output_path, 'r') as json_file:
-            courses = json.load(json_file)
-            self.assertIsNotNone(courses, "No courses extracted.")
-            self.assertGreater(len(courses), 0, "No course data extracted.")
-            for course in courses:
-                self.assertIn("Rubric Number", course)
-                self.assertIn("Course Name", course)
-                self.assertIn("Semesters Offered", course)
-            print("test_course_data_extraction passed successfully!")
+    def test_parsePrereqs(self):
+        parsePrereqs(self.pdf_path, self.json_output_path)
+        self.assertTrue(os.path.exists(self.json_output_path))
+        os.remove(self.json_output_path)
 
 if __name__ == "__main__":
     unittest.main()
